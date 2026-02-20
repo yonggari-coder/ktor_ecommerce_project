489fb9be119b6188b5df1e35c605dc3efff871ef
yong <yonggyun0120@gmail.com>
2026-02-21 00:15:24 +0900

    refactor: adapt existing repository for asynchronous db queries.

--- ktor-exposed-task-app/src/main/kotlin/model/FakeTaskRepository.kt
+++ ktor-exposed-task-app/src/main/kotlin/model/FakeTaskRepository.kt
@@ -8,24 +8,24 @@ class FakeTaskRepository : TaskRepository {
         Task("deploying", "Deploy my own project", Priority.High)
     )
 
-    override fun allTasks(): List<Task> = tasks
+    override suspend fun allTasks(): List<Task> = tasks
 
-    override fun tasksByPriority(priority: Priority) = tasks.filter{
+    override suspend fun tasksByPriority(priority: Priority) = tasks.filter{
         it.priority == priority
     }
 
-    override fun taskByName(name: String) = tasks.find {
+    override suspend fun taskByName(name: String) = tasks.find {
         it.name.equals(name, ignoreCase = true)
     }
 
-    override fun addTask(task: Task) {
+    override suspend fun addTask(task: Task) {
         if (taskByName(task.name) != null) {
             throw IllegalStateException("Cannot duplciate task names!")
         }
         tasks.add(task)
     }
 
-    override fun removeTask(name: String): Boolean {
+    override suspend fun removeTask(name: String): Boolean {
         return tasks.removeIf { it.name == name }
     }
 }
--- ktor-exposed-task-app/src/main/kotlin/model/TaskRepository.kt
+++ ktor-exposed-task-app/src/main/kotlin/model/TaskRepository.kt
@@ -1,10 +1,10 @@
 package com.example.model
 
 interface TaskRepository {
-    fun allTasks(): List<Task>
-    fun tasksByPriority(priority: Priority): List<Task>
-    fun taskByName(name: String): Task?
-    fun addTask(task: Task)
-    fun removeTask(name: String): Boolean
+    suspend fun allTasks(): List<Task>
+    suspend fun tasksByPriority(priority: Priority): List<Task>
+    suspend fun taskByName(name: String): Task?
+    suspend fun addTask(task: Task)
+    suspend fun removeTask(name: String): Boolean
 }
 
