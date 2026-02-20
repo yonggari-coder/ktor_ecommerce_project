e642b4a7bf62a9d8e7d7b22928caab2a92896895
yong <yonggyun0120@gmail.com>
2026-02-21 00:15:19 +0900

    feat: add Post route for submitting a task (ref: ktor docs - create a website)

--- ktor-task-web-app/src/main/kotlin/Templating.kt
+++ ktor-task-web-app/src/main/kotlin/Templating.kt
@@ -5,6 +5,7 @@ import com.example.model.TaskRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.Thymeleaf
@@ -27,6 +28,37 @@ fun Application.configureTemplating() {
                 call.respond(
                     ThymeleafContent("all-tasks", mapOf("tasks" to tasks))
                 )
+            }
+
+            post{
+                val formContent = call.receiveParameters()
+                val params = Triple(
+                    formContent["name"] ?: "",
+                    formContent["description"] ?: "",
+                    formContent["priority"] ?:""
+                )
+                if(params.toList().any {it.isEmpty()}) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                try {
+                    val priority = Priority.valueOf(params.third)
+                    TaskRepository.addTask(
+                        Task(
+                            params.first,
+                            params.second,
+                            priority
+                        )
+                    )
+                    val tasks = TaskRepository.allTasks()
+                    call.respond(
+                        ThymeleafContent("all-tasks", mapOf("tasks" to tasks))
+                    )
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
             }
 
             get("/byName") {
