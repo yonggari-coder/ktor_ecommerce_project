a22e18efd412c06bab7ac0fc47154568c4eda7f7
yong <yonggyun0120@gmail.com>
2026-02-21 00:13:55 +0900

    test: add unit tests to validate Ktor client and JSON structure

--- ktor-rest-task-app/src/test/kotlin/ApplicationTest.kt
+++ ktor-rest-task-app/src/test/kotlin/ApplicationTest.kt
@@ -1,10 +1,14 @@
 package com.example
 
+import com.example.model.Priority
+import com.example.model.Task
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.*
 
 class ApplicationTest {
 
@@ -16,6 +20,77 @@ class ApplicationTest {
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
+    }
+
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        application {
+            module()
+        }
+        val client = createClient{
+            install(ContentNegotiation){
+                json()
+            }
+        }
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val results = response.body<List<Task>>()
+
+        assertEquals(HttpStatusCode.OK, response.status)
+
+        val expectedTaskNames = listOf("gardening", "painting")
+        val actualTaskNames = results.map(Task::name)
+        assertContentEquals(expectedTaskNames, actualTaskNames)
+    }
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+
+    @Test
+    fun newTasksCanBeAdded() = testApplication {
+        application {
+            module()
+        }
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val task = Task("swimming", "Go to the beach", Priority.Low)
+        val response1 = client.post("/tasks") {
+            header(
+                HttpHeaders.ContentType,
+                ContentType.Application.Json
+            )
+
+            setBody(task)
+        }
+        assertEquals(HttpStatusCode.Created, response1.status)
+
+        val response2 = client.get("/tasks")
+        assertEquals(HttpStatusCode.OK, response2.status)
+
+        val taskNames = response2
+            .body<List<Task>>()
+            .map { it.name }
+
+        assertContains(taskNames, "swimming")
     }
 
 }
